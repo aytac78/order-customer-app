a86f176d4f2b21fc4ac172b52b88e211c64c7d9b
AYTAÇ GÖR <aytacgor@AYTAC-MacBook-Air.local>
2025-12-22 17:41:06 +0300

    Fix next.config to ignore all build errors

--- next.config.ts
+++ next.config.ts
@@ -1,1 +1,12 @@
-const nextConfig = { typescript: { ignoreBuildErrors: true } }; export default nextConfig;
+import type { NextConfig } from 'next'
+
+const nextConfig: NextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
+
+export default nextConfig
